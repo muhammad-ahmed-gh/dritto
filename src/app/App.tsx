226d5b305971b92914493d79bb687bd278d25e5f
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-03-22 22:41:17 +0200

    refactor: rebuild extension with React and Tailwind

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import "/src/styles/index.css";
+
+function App() {
+  return (
+    <div className="text-center flex flex-col justify-center h-screen">
+      <div className="text-[10rem]">🗺️</div>
+      <h1 className="font-bold text-[3rem]">معلش أنا حجبت الصفحة</h1>
+      <p className="text-[1.2rem] mt-[10px] text-[#777]">
+        زور صفحة تانية عشان الصفحة دي مشتتة
+      </p>
+    </div>
+  );
+}
+
+export default App;
