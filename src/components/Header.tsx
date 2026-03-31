7c2b78054b4923053c5c569d535c512088b5dcf5
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-03-31 19:29:03 +0200

    style: change header font size and replace About icon with text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
@@ -6,9 +5,11 @@ library.add(fas)
 export default function Header() {
   return (
     <header className="flex justify-between items-center bg-white h-[50px] px-[20px] border-b border-[#eee]">
-      <h1 className='italic select-none cursor-pointer font-bold text-[#666]'>Dritto</h1>
-      <button type="button" className='text-[#666] cursor-pointer'>
-        <FontAwesomeIcon icon={['fas', 'circle-info']} />
+      <h1 className='italic select-none cursor-pointer font-bold text-[#666] text-[17px]'>
+        Dritto
+      </h1>
+      <button type="button" className='text-[#666] cursor-pointer text-[14px]'>
+        About
       </button>
     </header>
   );
