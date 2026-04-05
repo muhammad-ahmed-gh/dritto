6d3ce4c9421b1a540afbb6ac63012e933a6c163d
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-05 22:05:09 +0200

    style: add glow effect to clock and removing border

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -14,7 +14,7 @@ export default function Clock(props: Props) {
 
 
   return (
-    <div className="clock w-[170px] h-[170px] flex justify-center items-center rounded-full border-[3px] border-primary mx-auto relative mt-[20px] select-none cursor-pointer transition-colors duration-300 hover:bg-surface-soft">
+    <div className="clock animate-glow w-[170px] h-[170px] flex justify-center items-center rounded-full mx-auto relative mt-[20px] select-none cursor-pointer transition-colors duration-300 hover:bg-surface-soft">
       <div className="container flex flex-col justify-center items-center">
         <span className="hours text-[50px] leading-[55px] font-bold">
           <span className="digit w-[34px] text-center inline-block">{hoursDigit1}</span>
