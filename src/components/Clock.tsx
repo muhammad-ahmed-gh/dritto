d40a437c3f32d833b6b0475a935dd7c9ef82d251
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-06 05:29:20 +0200

    feature: add a bouncing button on home tab and change Clock props name to time

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,28 +1,35 @@
 type Props = {
-  date: Date;
+  time: Date;
 };
 
 export default function Clock(props: Props) {
-  let hours = props.date.getHours();
+  let hours = props.time.getHours();
   hours = hours === 0 ? 12 : hours > 12 ? hours - 12 : hours;
   const hoursDigit1 = Math.trunc(hours / 10) % 10;
   const hoursDigit2 = hours % 10;
-  
-  const minutes = props.date.getMinutes();
+
+  const minutes = props.time.getMinutes();
   const minutesDigit1 = Math.trunc(minutes / 10) % 10;
   const minutesDigit2 = minutes % 10;
-
 
   return (
     <div className="clock animate-glow w-[170px] h-[170px] flex justify-center items-center rounded-full mx-auto relative mt-[20px] select-none cursor-pointer transition-colors duration-300 hover:bg-surface-soft">
       <div className="container flex flex-col justify-center items-center">
         <span className="hours text-[50px] leading-[55px] font-bold">
-          <span className="digit w-[34px] text-center inline-block">{hoursDigit1}</span>
-          <span className="digit w-[34px] text-center inline-block">{hoursDigit2}</span>
+          <span className="digit w-[34px] text-center inline-block">
+            {hoursDigit1}
+          </span>
+          <span className="digit w-[34px] text-center inline-block">
+            {hoursDigit2}
+          </span>
         </span>
         <span className="minutes text-[50px] leading-[55px] font-bold">
-          <span className="digit w-[34px] text-center inline-block">{minutesDigit1}</span>
-          <span className="digit w-[34px] text-center inline-block">{minutesDigit2}</span>
+          <span className="digit w-[34px] text-center inline-block">
+            {minutesDigit1}
+          </span>
+          <span className="digit w-[34px] text-center inline-block">
+            {minutesDigit2}
+          </span>
         </span>
         <span className="am absolute right-[20px] bottom-[33px]">
           {hours < 12 ? "AM" : "PM"}
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import Clock from "./Clock";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 export default function Home() {
-
   const [date, setDate] = useState(new Date());
   const hour = date.getHours();
 
   let greeting: string;
-  switch(true) {
+  switch (true) {
     case hour < 5:
       greeting = "Good night";
       break;
@@ -32,13 +33,20 @@ export default function Home() {
   }, []);
 
   return (
-      <main className="p-[20px] h-tab-height">
-        {greeting}
-        <Clock date={date} />
-        
-        <span className="text-[14px] text-text-muted block text-center mt-[15px]">
-          You have 2 pending tasks
-        </span>
-      </main>
+    <main className="p-[20px] h-tab-height">
+      {greeting}
+      <Clock time={date} />
+
+      <p className="text-[14px] text-text-muted block text-center mt-[20px]">
+        You have 2 pending tasks
+      </p>
+
+      <button
+        type="button"
+        className="flex justify-center items-center mx-auto mt-[20px] text-primary w-[35px] h-[35px] rounded-full bg-white hover:bg-[#fafafa] transition-colors duration-300 animate-bounce cursor-pointer border-[#e5e5e5] border [animation-duration:2s] "
+      >
+        <FontAwesomeIcon icon={faArrowDown} />
+      </button>
+    </main>
   );
 }
